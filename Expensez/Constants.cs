9a9531625c486782f121becd7848d4e11c45e406
David Rundqvist <david.rundqvist@gmail.com>
2021-04-28 21:31:34 +0200

    Fixed error in Constants.cs

--- Expensez/Constants.cs
+++ Expensez/Constants.cs
@@ -8,6 +8,6 @@ namespace Expensez {
     public static class Constants {
         public const string RootFolder = @"\\nasgul\David\Expenses";
 
-        public static CategoryPresentation DefaultCategory { get; } = new(new Category("Övrigt", "White", new[] { ".*" }));
+        public static Category DefaultCategory { get; } = new Category("Övrigt", "White", new[] { ".*" });
     }
 }
