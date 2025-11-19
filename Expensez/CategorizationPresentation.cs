89ccc7f3a05178eed52bc85a61341235c068834f
David Rundqvist <david.rundqvist@gmail.com>
2025-11-19 18:46:01 +0100

    Categories list

--- Expensez/CategorizationPresentation.cs
+++ Expensez/CategorizationPresentation.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
 using Expensez.Commands;
@@ -5,15 +6,20 @@ using Expensez.Commands;
 namespace Expensez;
 
 public class CategorizationPresentation : INotifyPropertyChanged
-{    
+{
     public event PropertyChangedEventHandler? PropertyChanged;
 
     private readonly ICommand _newCategoryCommand;
 
     public ICommand NewCategoryCommand => _newCategoryCommand;
+    public ObservableCollection<CategoryPresentation> Categories { get; }
 
     public CategorizationPresentation()
     {
         _newCategoryCommand = new NewCategoryCommand(this);
+        Categories = [
+            new(new("Mat", "Blue")),
+            new(new("Bil", "Orange")),
+            new(new("Kläder", "Green"))];
     }
 }
---
+++ Expensez/CategoryPresentation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Expensez {
+    public class CategoryPresentation : INotifyPropertyChanged {
+        private readonly Category _category;
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public CategoryPresentation(Category category) {
+            _category = category;
+        }
+
+        public string Name {
+            get => _category.Name;
+            set {
+                if (_category.Name != value) {
+                    _category.Name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+                }
+            }
+        }
+
+        public bool IsSelected { get; set; } = false;
+
+
+        public string Color {
+            get => _category.Color;
+            set {
+                if (_category.Color != value) {
+                    _category.Color = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Color)));
+                }
+            }
+        }
+
+        public string[] Patterns {
+            get => _category.Patterns;
+            set => _category.Patterns = value;
+        }
+        public Category Category => _category;
+
+    }
+}
